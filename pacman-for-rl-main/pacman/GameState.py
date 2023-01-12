705ac851054e1494c88c7aa61a0aef2f85d75381
Bartek <michasbartek@gmail.com>
2023-01-12 21:11:12 +0100

    multiagent pycman

---
+++ pacman-for-rl-main/pacman/GameState.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import List, Dict, Any, Set, Tuple
+from .Position import Position
+
+"""
+All info you might need, I hope.
+"""
+
+
+@dataclass
+class GameState:
+    you: Dict[str, Any]
+    other_pacmans: List[Dict[str, Any]]
+    ghosts: List[Dict[str, Any]]
+    points: Set[Position]
+    big_points: Set[Position]
+    phasing_points: Set[Position]
+    double_points: Set[Position]
+    indestructible_points: Set[Position]
+    big_big_points: Set[Position]
+    walls: Set[Position]
+    board_size: Tuple[int, int]
